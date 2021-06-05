cd0c5e2017bfe691bf07bfce9487381649395983
TANISHA-byt <83496848+TANISHA-byt@users.noreply.github.com>
2021-06-05 15:06:22 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://language-translation.p.rapidapi.com/translateLanguage/detect-language"
+
+querystring = {"text":"Привет, мой дорогой друг!"}
+
+headers = {
+    'x-rapidapi-key': "5d797ab107mshe668f26bd044e64p1ffd34jsnf47bfa9a8ee4",
+    'x-rapidapi-host': "language-translation.p.rapidapi.com"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
